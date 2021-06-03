c885bc038bff6a57cdc54256a0b2c9a2314e01e8
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2021-06-03 16:38:36 +0700

    flatMapFirst.kt

--- src/commonMain/kotlin/com/hoc081098/flowext/flatMapFirst.kt
+++ src/commonMain/kotlin/com/hoc081098/flowext/flatMapFirst.kt
@@ -1,13 +1,10 @@
 package com.hoc081098.flowext
 
-import kotlinx.coroutines.CancellationException
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.cancel
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.launch
 
 @ExperimentalCoroutinesApi
 fun <T, R> Flow<T>.flatMapFirst(transform: suspend (value: T) -> Flow<R>): Flow<R> =
@@ -17,9 +14,11 @@ fun <T, R> Flow<T>.flatMapFirst(transform: suspend (value: T) -> Flow<R>): Flow<
 fun <T> Flow<Flow<T>>.flattenFirst(): Flow<T> = channelFlow {
     val outerScope = this
     val busy = AtomicBoolean(false)
+
     collect { inner ->
         if (busy.compareAndSet(expect = false, update = true)) {
-            launch {
+            // Do not pay for dispatch here, it's never necessary
+            launch(start = CoroutineStart.UNDISPATCHED) {
                 try {
                     inner.collect { outerScope.send(it) }
                     busy.value = false
