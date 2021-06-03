d971bcc91861f47d200265904ec826dcfdf145b4
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2021-06-03 16:48:34 +0700

    wip

---
+++ src/commonTest/kotlin/com/hoc081098/flowext/FlatMapFirst.kt
@@ -0,0 +1,11 @@
+import com.hoc081098.flowext.flatMapFirst
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.*
+import kotlin.test.Test
+
+class FlatMapFirst {
+
+}
