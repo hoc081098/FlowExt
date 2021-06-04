858e235465489d41f19654b3d86fe5bb749320ef
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2021-06-04 15:54:22 +0700

    remove macOS-latest when testing

--- README.md
+++ README.md
@@ -3,5 +3,12 @@ Kotlinx Coroutine Flow Extensions
 
 [![CI](https://github.com/hoc081098/FlowExt/actions/workflows/build.yml/badge.svg)](https://github.com/hoc081098/FlowExt/actions/workflows/build.yml)
 
-- [flatMapFirst](), [flattenFirst]() (similar to [RxJS exhaustMap](), [RxSwift flatMapFirst]())
-- [range]()
+-   [flatMapFirst]()/[exhaustMap]() (similar to [RxJS exhaustMap](https://rxjs.dev/api/operators/exhaustMap), [RxSwift flatMapFirst](https://github.com/ReactiveX/RxSwift/blob/1a1fa37b0d08e0f99ffa41f98f340e8bc60c35c4/RxSwift/Observables/Merge.swift#L37))
+    Projects each source value to a Flow which is merged in the output Flow only if the previous projected Flow has completed.
+    If value is received while there is some projected Flow sequence being merged it will simply be ignored.
+
+-   [flattenFirst]()/[exhaustAll]() (similar to [RxJs exhaustAll](https://rxjs.dev/api/operators/exhaustAll))
+    Converts a higher-order Flow into a first-order Flow by dropping inner Flow while the previous inner Flow has not yet completed.
+
+-   [range]() (similar to [RxJS range](https://rxjs.dev/api/index/function/range))
+    Creates a Flow that emits a sequence of numbers within a specified range.
--- src/commonMain/kotlin/com/hoc081098/flowext/flatMapFirst.kt
+++ src/commonMain/kotlin/com/hoc081098/flowext/flatMapFirst.kt
@@ -5,15 +5,34 @@ import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.buffer
 import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.produceIn
 import kotlinx.coroutines.launch
 
+/**
+ * Projects each source value to a [Flow] which is merged in the output [Flow] only if the previous projected [Flow] has completed.
+ * If value is received while there is some projected [Flow] sequence being merged it will simply be ignored.
+ *
+ * This method is a shortcut for `map(transform).flattenFirst()`. See [flattenFirst].
+ *
+ * ### Operator fusion
+ *
+ * Applications of [flowOn], [buffer], and [produceIn] _after_ this operator are fused with
+ * its concurrent merging so that only one properly configured channel is used for execution of merging logic.
+ *
+ * @param transform A transform function to apply to value that was observed while no Flow is executing in parallel.
+ */
 @ExperimentalCoroutinesApi
 public fun <T, R> Flow<T>.flatMapFirst(transform: suspend (value: T) -> Flow<R>): Flow<R> =
   map(transform).flattenFirst()
 
+/**
+ * Converts a higher-order [Flow] into a first-order [Flow] by dropping inner [Flow] while the previous inner [Flow] has not yet completed.
+ */
 @ExperimentalCoroutinesApi
 public fun <T> Flow<Flow<T>>.flattenFirst(): Flow<T> = channelFlow {
   val outerScope = this
@@ -34,3 +53,16 @@ public fun <T> Flow<Flow<T>>.flattenFirst(): Flow<T> = channelFlow {
     }
   }
 }
+
+/**
+ * This function is an alias to [flatMapFirst] operator.
+ */
+@ExperimentalCoroutinesApi
+public fun <T, R> Flow<T>.exhaustMap(transform: suspend (value: T) -> Flow<R>): Flow<R> =
+  flatMapFirst(transform)
+
+/**
+ * This function is an alias to [flattenFirst] operator.
+ */
+@ExperimentalCoroutinesApi
+public fun <T> Flow<Flow<T>>.exhaustAll(): Flow<T> = flattenFirst()
