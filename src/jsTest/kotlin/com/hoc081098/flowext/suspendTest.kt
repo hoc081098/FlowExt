edea3471084ec17d5918467a929fab638d5fa1c0
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2021-06-04 23:53:16 +0700

    local GlobalScope

--- src/jsTest/kotlin/com/hoc081098/flowext/suspendTest.kt
+++ src/jsTest/kotlin/com/hoc081098/flowext/suspendTest.kt
@@ -1,9 +1,15 @@
 package com.hoc081098.flowext
 
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.promise
+
+private object GlobalScope : CoroutineScope {
+  override val coroutineContext: CoroutineContext
+    get() = EmptyCoroutineContext
+}
 
 @DelicateCoroutinesApi
 actual fun suspendTest(block: suspend CoroutineScope.() -> Unit): dynamic {
