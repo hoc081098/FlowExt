0dd6e12dc26f912e9a7ae8ea936463341335cc2f
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2021-06-04 11:10:16 +0700

    docs(range)

--- src/commonMain/kotlin/com/hoc081098/flowext/range.kt
+++ src/commonMain/kotlin/com/hoc081098/flowext/range.kt
@@ -3,6 +3,11 @@ package com.hoc081098.flowext
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
+/**
+ * Creates a [Flow] that emits a sequence of numbers within a specified range.
+ * @param start The value of the first integer in the sequence.
+ * @param count The number of sequential integers to generate.
+ */
 fun range(start: Int, count: Int): Flow<Int> = flow {
     repeat(count) { emit(it + start) }
 }
