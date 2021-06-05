083d57c1d7cbc11f7b09cbcaaffadb546a125900
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2021-06-05 21:46:34 +0700

    update takeUntil.kt

--- src/commonMain/kotlin/com/hoc081098/flowext/takeUntil.kt
+++ src/commonMain/kotlin/com/hoc081098/flowext/takeUntil.kt
@@ -3,7 +3,6 @@ package com.hoc081098.flowext
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.take
@@ -12,16 +11,18 @@ import kotlinx.coroutines.launch
 @ExperimentalCoroutinesApi
 public fun <T, R> Flow<T>.takeUntil(notifier: Flow<R>): Flow<T> = flow {
   coroutineScope {
-    launch {
-      notifier.take(1).collect()
-      throw ClosedException
-    }
+    try {
+      val job = launch {
+        notifier.take(1).collect()
+        throw ClosedException
+      }
 
-    collect { emit(it) }
-    throw ClosedException
+      collect { return@collect emit(it) }
+      job.cancel()
+    } catch (e: ClosedException) {
+      // no-op
+    }
   }
-}.catch {
-  if (it !is ClosedException) throw it
 }
 
 private object ClosedException : Exception()
