ce8bdecfae591e742acbb9e41c0c7acdf7426868
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2021-06-08 15:53:35 +0700

    try to fix tests

--- src/jsTest/kotlin/com/hoc081098/flowext/suspendTest.kt
+++ src/jsTest/kotlin/com/hoc081098/flowext/suspendTest.kt
@@ -3,7 +3,6 @@ package com.hoc081098.flowext
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.promise
 
 private object GlobalScope : CoroutineScope {
@@ -11,7 +10,5 @@ private object GlobalScope : CoroutineScope {
     get() = EmptyCoroutineContext
 }
 
-@DelicateCoroutinesApi
-actual fun suspendTest(block: suspend CoroutineScope.() -> Unit): dynamic {
-  return GlobalScope.promise(block = block)
-}
+actual fun suspendTest(block: suspend CoroutineScope.() -> Unit): dynamic =
+  GlobalScope.promise(block = block)
