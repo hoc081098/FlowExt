81720bfb6bc1b120568879ef519d1602424d8b42
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2021-06-10 16:41:35 +0700

    update tests

--- src/jvmTest/kotlin/com/hoc081098/flowext/suspendTest.kt
+++ src/jvmTest/kotlin/com/hoc081098/flowext/suspendTest.kt
@@ -1,6 +1,11 @@
 package com.hoc081098.flowext
 
+import java.util.concurrent.Executors
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.runBlocking
 
-actual fun suspendTest(block: suspend CoroutineScope.() -> Unit) = runBlocking(block = block)
+actual fun suspendTest(block: suspend CoroutineScope.() -> Unit) = runBlocking(
+  block = block,
+  context = Executors.newSingleThreadExecutor().asCoroutineDispatcher(),
+)
