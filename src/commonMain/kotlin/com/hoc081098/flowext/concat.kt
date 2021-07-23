10ad4801fdd14b11763705588a5eddef70150116
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2021-07-23 11:16:21 +0700

    add concat, interval

---
+++ src/commonMain/kotlin/com/hoc081098/flowext/concat.kt
@@ -0,0 +1,71 @@
+package com.hoc081098.flowext
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flow
+
+public fun <T> concat(flow1: Flow<T>, flow2: Flow<T>): Flow<T> = flow {
+  emitAll(flow1)
+  emitAll(flow2)
+}
+
+public fun <T> concat(flow1: Flow<T>, flow2: Flow<T>, flow3: Flow<T>): Flow<T> = flow {
+  emitAll(flow1)
+  emitAll(flow2)
+  emitAll(flow3)
+}
+
+public fun <T> concat(flow1: Flow<T>, flow2: Flow<T>, flow3: Flow<T>, flow4: Flow<T>): Flow<T> =
+  flow {
+    emitAll(flow1)
+    emitAll(flow2)
+    emitAll(flow3)
+    emitAll(flow4)
+  }
+
+public fun <T> concat(
+  flow1: Flow<T>,
+  flow2: Flow<T>,
+  flow3: Flow<T>,
+  flow4: Flow<T>,
+  flow5: Flow<T>
+): Flow<T> =
+  flow {
+    emitAll(flow1)
+    emitAll(flow2)
+    emitAll(flow3)
+    emitAll(flow4)
+    emitAll(flow5)
+  }
+
+public fun <T> Flow<T>.concatWith(flow: Flow<T>): Flow<T> = concat(this, flow)
+
+public fun <T> Flow<T>.concatWith(flow1: Flow<T>, flow2: Flow<T>): Flow<T> =
+  concat(this, flow1, flow2)
+
+public fun <T> Flow<T>.concatWith(flow1: Flow<T>, flow2: Flow<T>, flow3: Flow<T>): Flow<T> =
+  concat(this, flow1, flow2, flow3)
+
+public fun <T> Flow<T>.concatWith(
+  flow1: Flow<T>,
+  flow2: Flow<T>,
+  flow3: Flow<T>,
+  flow4: Flow<T>
+): Flow<T> =
+  concat(this, flow1, flow2, flow3, flow4)
+
+//
+//
+//
+
+public fun <T> concat(vararg flows: Flow<T>): Flow<T> {
+  return when {
+    flows.isEmpty() -> emptyFlow()
+    flows.size == 1 -> flows[0]
+    else -> flow { flows.forEach { emitAll(it) } }
+  }
+}
+
+public fun <T> Flow<T>.concatWith(vararg others: Flow<T>): Flow<T> = concat(this, *others)
+
---
+++ src/commonMain/kotlin/com/hoc081098/flowext/interval.kt
@@ -0,0 +1,44 @@
+package com.hoc081098.flowext
+
+import kotlin.time.Duration
+import kotlin.time.ExperimentalTime
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+@ExperimentalTime
+public fun interval(
+  initialDelay: Duration,
+  delay: Duration,
+): Flow<Long> {
+  require(initialDelay >= Duration.ZERO) { "Expected non-negative delay, but has $initialDelay ms" }
+  require(delay >= Duration.ZERO) { "Expected non-negative delay, but has $delay ms" }
+
+  return flow {
+    delay(initialDelay)
+
+    var count = 0L
+    while (true) {
+      emit(count++)
+      delay(delay)
+    }
+  }
+}
+
+public fun interval(
+  initialDelayMillis: Long,
+  delayMillis: Long,
+): Flow<Long> {
+  require(initialDelayMillis >= 0) { "Expected non-negative delay, but has $initialDelayMillis ms" }
+  require(delayMillis >= 0) { "Expected non-negative delay, but has $delayMillis ms" }
+
+  return flow {
+    delay(initialDelayMillis)
+
+    var count = 0L
+    while (true) {
+      emit(count++)
+      delay(delayMillis)
+    }
+  }
+}
