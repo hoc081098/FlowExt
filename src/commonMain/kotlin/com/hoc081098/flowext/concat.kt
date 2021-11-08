0a565cbd50f8d798483689d0b92a464126fcfbd7
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2021-11-09 01:04:55 +0700

    add startWith* [skip ci]

--- src/commonMain/kotlin/com/hoc081098/flowext/concat.kt
+++ src/commonMain/kotlin/com/hoc081098/flowext/concat.kt
@@ -4,6 +4,11 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onStart
+
+//
+// concat
+//
 
 public fun <T> concat(flow1: Flow<T>, flow2: Flow<T>): Flow<T> = flow {
   emitAll(flow1)
@@ -39,6 +44,18 @@ public fun <T> concat(
     emitAll(flow5)
   }
 
+public fun <T> concat(vararg flows: Flow<T>): Flow<T> {
+  return when {
+    flows.isEmpty() -> emptyFlow()
+    flows.size == 1 -> flows[0]
+    else -> flow { flows.forEach { emitAll(it) } }
+  }
+}
+
+//
+// concatWith
+//
+
 public fun <T> Flow<T>.concatWith(flow: Flow<T>): Flow<T> = concat(this, flow)
 
 public fun <T> Flow<T>.concatWith(flow1: Flow<T>, flow2: Flow<T>): Flow<T> =
@@ -55,16 +72,15 @@ public fun <T> Flow<T>.concatWith(
 ): Flow<T> =
   concat(this, flow1, flow2, flow3, flow4)
 
-//
-//
-//
-
-public fun <T> concat(vararg flows: Flow<T>): Flow<T> {
-  return when {
-    flows.isEmpty() -> emptyFlow()
-    flows.size == 1 -> flows[0]
-    else -> flow { flows.forEach { emitAll(it) } }
-  }
-}
-
 public fun <T> Flow<T>.concatWith(vararg others: Flow<T>): Flow<T> = concat(this, *others)
+
+//
+// startWith
+//
+
+public fun <T> Flow<T>.startWithItem(item: T): Flow<T> = onStart { emit(item) }
+
+public fun <T> Flow<T>.startWithArray(vararg items: T): Flow<T> =
+  onStart { items.forEach { emit(it) } }
+
+public fun <T> Flow<T>.startWithFlow(other: Flow<T>): Flow<T> = concat(other, this)
