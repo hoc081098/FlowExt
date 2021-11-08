af28bcd771264080a8185e9daa98b636819be70f
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2021-11-09 02:59:45 +0700

    done(bufferCount)

--- src/commonMain/kotlin/com/hoc081098/flowext/bufferCount.kt
+++ src/commonMain/kotlin/com/hoc081098/flowext/bufferCount.kt
@@ -4,25 +4,88 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 
+/**
+ * Buffers the source [Flow] values until the size hits the maximum [bufferSize] given.
+ *
+ * @param bufferSize The maximum size of the buffer emitted.
+ * @param startBufferEvery Optional. Default is null.
+ * Interval at which to start a new buffer.
+ * For example if [startBufferEvery] is 2, then a new buffer will be started on every other value from the source.
+ * A new buffer is started at the beginning of the source by default.
+ */
 public fun <T> Flow<T>.bufferCount(
   bufferSize: Int,
   startBufferEvery: Int? = null,
 ): Flow<List<T>> {
+  // If no `startBufferEvery` value was supplied, then we're
+  // opening and closing on the bufferSize itself.
+  return if (startBufferEvery == null || bufferSize == startBufferEvery) {
+    bufferExact(bufferSize)
+  } else {
+    bufferSkip(bufferSize, startBufferEvery)
+  }
+}
+
+private fun <T> Flow<T>.bufferSkip(bufferSize: Int, skip: Int): Flow<List<T>> {
   return flow {
-    val buffer = mutableListOf<T>()
+    val buffers = ArrayDeque<MutableList<T>>()
+    var index = 0
+
+    try {
+      collect { element ->
+        // Check to see if we need to start a buffer.
+        // This will start one at the first value, and then
+        // a new one every N after that.
+        if (index++ % skip == 0) {
+          buffers += mutableListOf<T>()
+        }
+
+        // Push our value into our active buffers.
+        buffers.iterator().let { iterator ->
+          iterator.forEach { buffer ->
+            buffer += element
+
+            // Check to see if we're over the bufferSize
+            // if we are, record it so we can emit it later.
+            // If we emitted it now and removed it, it would
+            // mutate the `buffers` array while we're looping
+            // over it.
+            if (buffer.size >= bufferSize) {
+              // We have found some buffers that are over the
+              // `bufferSize`. Emit them, and remove them from our
+              // buffers list.
+              iterator.remove()
+              emit(buffer.toList())
+            }
+          }
+        }
+      }
+
+      // When the source completes, emit all of our
+      // active buffers.
+      buffers.forEach { emit(it.toList()) }
+    } finally {
+      buffers.clear()
+    }
+  }
+}
+
+private fun <T> Flow<T>.bufferExact(bufferSize: Int): Flow<List<T>> {
+  return flow {
+    var buffer: MutableList<T> = mutableListOf()
 
     collect { element ->
-      if (buffer.size < bufferSize) buffer += element
+      buffer += element
 
-      if (buffer.size == bufferSize) {
-        emit(buffer.toList())
-        buffer.clear()
+      if (buffer.size >= bufferSize) {
+        emit(buffer)
+        buffer = mutableListOf()
       }
     }
 
+    // Emits remaining buffer
     if (buffer.isNotEmpty()) {
-      emit(buffer.toList())
-      buffer.clear()
+      emit(buffer)
     }
   }
 }
--- src/commonTest/kotlin/com/hoc081098/flowext/BufferCountTest.kt
+++ src/commonTest/kotlin/com/hoc081098/flowext/BufferCountTest.kt
@@ -2,13 +2,20 @@ package com.hoc081098.flowext
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.take
 import kotlin.test.Test
+import kotlin.test.assertContentEquals
+import kotlin.test.assertIs
 
 @ExperimentalCoroutinesApi
 @InternalCoroutinesApi
 class BufferCountTest {
   @Test
-  fun basic() = suspendTest {
+  fun testBufferCount_shouldEmitBuffersAtBufferSize() = suspendTest {
     range(0, 10)
       .bufferCount(3)
       .test(
@@ -20,5 +27,104 @@ class BufferCountTest {
           Event.Complete,
         )
       )
+  }
+
+  @Test
+  fun testBufferCount_shouldEmitBuffersAtBufferSizeWithStartBufferEvery() = suspendTest {
+    range(0, 8)
+      .bufferCount(3, 1)
+      .test(
+        listOf(
+          Event.Value(listOf(0, 1, 2)),
+          Event.Value(listOf(1, 2, 3)),
+          Event.Value(listOf(2, 3, 4)),
+          Event.Value(listOf(3, 4, 5)),
+          Event.Value(listOf(4, 5, 6)),
+          Event.Value(listOf(5, 6, 7)),
+          Event.Value(listOf(6, 7)),
+          Event.Value(listOf(7)),
+          Event.Complete,
+        )
+      )
+
+    range(0, 10)
+      .bufferCount(2, 4)
+      .test(
+        listOf(
+          Event.Value(listOf(0, 1)),
+          Event.Value(listOf(4, 5)),
+          Event.Value(listOf(8, 9)),
+          Event.Complete,
+        )
+      )
+  }
+
+  @Test
+  fun testBufferCount_shouldThrowExceptionWithFailureUpStream() = suspendTest {
+    flow<Int> { throw RuntimeException("Broken!") }
+      .bufferCount(2)
+      .test(null) {
+        assertIs<RuntimeException>(it.single().throwableOrThrow())
+      }
+
+    flow<Int> { throw RuntimeException("Broken!") }
+      .bufferCount(2, 1)
+      .test(null) {
+        assertIs<RuntimeException>(it.single().throwableOrThrow())
+      }
+  }
+
+  @Test
+  fun testBufferCount_testCancellation() = suspendTest {
+    range(0, 10)
+      .bufferCount(4)
+      .take(2)
+      .test(
+        listOf(
+          Event.Value(listOf(0, 1, 2, 3)),
+          Event.Value(listOf(4, 5, 6, 7)),
+          Event.Complete,
+        )
+      )
+  }
+
+  @Test
+  fun testBufferCount_testCancellationWithStartBufferEvery() = suspendTest {
+    range(0, 10)
+      .bufferCount(4, 2)
+      .take(2)
+      .test(
+        listOf(
+          Event.Value(listOf(0, 1, 2, 3)),
+          Event.Value(listOf(2, 3, 4, 5)),
+          Event.Complete,
+        )
+      )
+  }
+
+  @Test
+  fun testBufferCount_shouldBufferProperly() = suspendTest {
+    val flow = MutableSharedFlow<Int>(extraBufferCapacity = 64)
+
+    val results = mutableListOf<List<Int>>()
+    val job = flow.bufferCount(3, 1).onEach {
+      results += it
+      if (it == listOf(1, 2, 3)) {
+        flow.tryEmit(4)
+      }
+    }.launchIn(this)
+
+    flow.tryEmit(1)
+    flow.tryEmit(2)
+    flow.tryEmit(3)
+
+    job.cancel()
+    assertContentEquals(
+      results,
+      listOf(
+        listOf(1, 2, 3),
+        listOf(2, 3, 4),
+      )
+    )
   }
 }
