a0e1b3912e8412afe32f75804c53154acc4ba0bd
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2021-11-27 13:29:21 +0700

    add retryWhenWithDelayStrategy

---
+++ src/commonMain/kotlin/com/hoc081098/flowext/retryWhenWithDelayStrategy.kt
@@ -0,0 +1,114 @@
+package com.hoc081098.flowext
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.retryWhen
+import kotlin.math.pow
+import kotlin.time.Duration
+import kotlin.time.ExperimentalTime
+
+// @ExperimentalTime
+// public fun <T> Flow<T>.retryWithExponentialBackoff(
+//  initialDelay: Duration,
+//  factor: Double,
+//  maxAttempt: Long = Long.MAX_VALUE,
+//  maxDelay: Duration = Duration.INFINITE,
+//  predicate: suspend (cause: Throwable) -> Boolean = { true }
+// ): Flow<T> {
+//  require(maxAttempt > 0) { "Expected positive amount of maxAttempt, but had $maxAttempt" }
+//  return retryWhenWithExponentialBackoff(
+//    initialDelay = initialDelay,
+//    factor = factor,
+//    maxDelay = maxDelay
+//  ) { cause, attempt -> attempt < maxAttempt && predicate(cause) }
+// }
+//
+// @ExperimentalTime
+// public fun <T> Flow<T>.retryWhenWithExponentialBackoff(
+//  initialDelay: Duration,
+//  factor: Double,
+//  maxDelay: Duration = Duration.INFINITE,
+//  predicate: suspend FlowCollector<T>.(cause: Throwable, attempt: Long) -> Boolean
+// ): Flow<T> = flow {
+//  var currentDelay = initialDelay
+//
+//  retryWhen { cause, attempt ->
+//    predicate(cause, attempt).also {
+//      if (it) {
+//        delay(currentDelay)
+//        currentDelay = (currentDelay * factor).coerceAtMost(maxDelay)
+//      }
+//    }
+//  }.let { emitAll(it) }
+// }
+
+@ExperimentalTime
+public fun <T> Flow<T>.retryWhenWithDelayStrategy(
+  strategy: DelayStrategy,
+  predicate: suspend FlowCollector<T>.(cause: Throwable, attempt: Long) -> Boolean
+): Flow<T> = retryWhen { cause, attempt ->
+  predicate(cause, attempt).also {
+    if (it) {
+      delay(strategy.duration(cause, attempt))
+    }
+  }
+}
+
+public fun interface DelayStrategy {
+  public fun duration(cause: Throwable, attempt: Long): Duration
+
+  public object Immediate : DelayStrategy {
+    override fun duration(cause: Throwable, attempt: Long): Duration = Duration.ZERO
+  }
+
+  public class Constant(private val duration: Duration) : DelayStrategy {
+    override fun duration(cause: Throwable, attempt: Long): Duration = duration
+  }
+
+  public class Exponential(
+    private val initialDelay: Duration,
+    private val factor: Double,
+    private val maxDelay: Duration,
+  ) : DelayStrategy {
+    override fun duration(cause: Throwable, attempt: Long): Duration = (
+      if (attempt == 0L) {
+        initialDelay
+      } else {
+        initialDelay * factor.pow(attempt.toDouble())
+      }
+      ).coerceAtLeast(maxDelay)
+  }
+}
+
+@ExperimentalTime
+public fun <T> Flow<T>.retryWithExponentialBackoff(
+  initialDelay: Duration,
+  factor: Double,
+  maxAttempt: Long = Long.MAX_VALUE,
+  maxDelay: Duration = Duration.INFINITE,
+  predicate: suspend (cause: Throwable) -> Boolean = { true }
+): Flow<T> {
+  require(maxAttempt > 0) { "Expected positive amount of maxAttempt, but had $maxAttempt" }
+
+  return retryWhenWithExponentialBackoff(
+    initialDelay = initialDelay,
+    factor = factor,
+    maxDelay = maxDelay
+  ) { cause, attempt -> attempt < maxAttempt && predicate(cause) }
+}
+
+@ExperimentalTime
+public fun <T> Flow<T>.retryWhenWithExponentialBackoff(
+  initialDelay: Duration,
+  factor: Double,
+  maxDelay: Duration = Duration.INFINITE,
+  predicate: suspend FlowCollector<T>.(cause: Throwable, attempt: Long) -> Boolean
+): Flow<T> = retryWhenWithDelayStrategy(
+  strategy = DelayStrategy.Exponential(
+    initialDelay = initialDelay,
+    factor = factor,
+    maxDelay = maxDelay,
+  ),
+  predicate = predicate,
+)
--- src/commonTest/kotlin/com/hoc081098/flowext/MapIndexedTest.kt
+++ src/commonTest/kotlin/com/hoc081098/flowext/MapIndexedTest.kt
@@ -1,9 +1,14 @@
 package com.hoc081098.flowext
 
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.take
 import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertIs
 
 @InternalCoroutinesApi
 @ExperimentalCoroutinesApi
@@ -18,6 +23,47 @@ class MapIndexedTest {
           Event.Value(1 to 2),
           Event.Value(2 to 3),
           Event.Value(3 to 4),
+          Event.Complete,
+        )
+      )
+  }
+
+  @Test
+  fun upstreamError() = suspendTest {
+    val throwable = RuntimeException()
+
+    flow<Int> { throw throwable }
+      .mapIndexed { index, value -> index to value }
+      .test(listOf(Event.Error(throwable)))
+  }
+
+  @Test
+  fun cancellation() = suspendTest {
+    flow {
+      repeat(5) {
+        if (it == 2) throw CancellationException("")
+        else emit(it)
+      }
+    }
+      .mapIndexed { index, value -> index to value }
+      .test(null) {
+        assertEquals(
+          listOf(
+            Event.Value(0 to 0),
+            Event.Value(1 to 1),
+          ),
+          it.take(2)
+        )
+        assertIs<CancellationException>(it.drop(2).single().errorOrThrow())
+      }
+
+    flowOf(0, 1, 2, 3, 4, 5, 6)
+      .mapIndexed { index, value -> index to value }
+      .take(2)
+      .test(
+        listOf(
+          Event.Value(0 to 0),
+          Event.Value(1 to 1),
           Event.Complete,
         )
       )
