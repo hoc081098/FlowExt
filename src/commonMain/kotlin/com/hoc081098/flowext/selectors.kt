2e8cf0b86041ba9c85c924946eaefbadef5a063f
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2022-11-07 11:03:52 +0700

    feat(selectors): docs, api dump

--- src/commonMain/kotlin/com/hoc081098/flowext/selectors.kt
+++ src/commonMain/kotlin/com/hoc081098/flowext/selectors.kt
@@ -35,7 +35,7 @@ import kotlinx.coroutines.flow.map
 private typealias SubStateT = Any?
 
 private fun <State, Result> Flow<State>.selectInternal(
-  selectors: Array<out suspend (State) -> SubStateT>,
+  selectors: Array<Selector<State, SubStateT>>,
   projector: suspend (Array<SubStateT>) -> Result
 ): Flow<Result> {
   require(selectors.isNotEmpty()) { "selectors must not be empty" }
@@ -71,22 +71,68 @@ private fun <State, Result> Flow<State>.selectInternal(
 
 // -------------------------------------------------------------------------------------------------
 
-public fun <State, Result> Flow<State>.select(selector: (State) -> Result): Flow<Result> =
+/**
+ * Inspirited by [NgRx memoized selector](https://ngrx.io/guide/store/selectors).
+ *
+ * Selectors are pure functions used for obtaining slices of a Flow of state.
+ * `FlowExt` provides a few helper functions for optimizing this selection.
+ *
+ * - Selectors can compute derived data, to store the minimal possible state.
+ * - Selectors are efficient. A selector is not recomputed unless one of its arguments changes.
+ * - When using the [select] functions, it will keep track of the latest arguments in which your selector function was invoked.
+ *   Because selectors are pure functions, the last result can be returned
+ *   when the arguments match without re-invoking your selector function.
+ *   This can provide performance benefits, particularly with selectors that perform expensive computation.
+ *   This practice is known as memoization.
+ */
+public typealias Selector<State, SubState> = suspend (State) -> SubState
+
+/**
+ * Select a sub-state from the [State] and emit it if it is different from the previous one.
+ *
+ * @param selector A function that takes the [State] and returns a sub-state.
+ */
+public fun <State, Result> Flow<State>.select(selector: Selector<State, Result>): Flow<Result> =
   map(selector).distinctUntilChanged()
 
+/**
+ * Select two sub-states from the source [Flow] and combine them into a [Result].
+ *
+ * The [projector] will be invoked only when one of the sub-states is changed.
+ * The returned flow will emit the result of [projector],
+ * and all subsequent results repetitions of the same value are filtered out
+ * (same as [distinctUntilChanged]).
+ *
+ * @param selector1 The first selector to be used to select first sub-states.
+ * @param selector2 The second selector to be used to select second sub-states.
+ * @param projector The projector to be used to combine the sub-states into a result.
+ */
 public fun <State, SubState1, SubState2, Result> Flow<State>.select(
-  selector1: suspend (State) -> SubState1,
-  selector2: suspend (State) -> SubState2,
+  selector1: Selector<State, SubState1>,
+  selector2: Selector<State, SubState2>,
   projector: suspend (subState1: SubState1, subState2: SubState2) -> Result
 ): Flow<Result> = selectInternal(
   selectors = arrayOf(selector1, selector2),
   projector = { projector(it[0] as SubState1, it[1] as SubState2) }
 )
 
+/**
+ * Select three sub-states from the source [Flow] and combine them into a [Result].
+ *
+ * The [projector] will be invoked only when one of the sub-states is changed.
+ * The returned flow will emit the result of [projector],
+ * and all subsequent results repetitions of the same value are filtered out
+ * (same as [distinctUntilChanged]).
+ *
+ * @param selector1 The first selector to be used to select first sub-states.
+ * @param selector2 The second selector to be used to select second sub-states.
+ * @param selector3 The third selector to be used to select third sub-states.
+ * @param projector The projector to be used to combine the sub-states into a result.
+ */
 public fun <State, SubState1, SubState2, SubState3, Result> Flow<State>.select(
-  selector1: suspend (State) -> SubState1,
-  selector2: suspend (State) -> SubState2,
-  selector3: suspend (State) -> SubState3,
+  selector1: Selector<State, SubState1>,
+  selector2: Selector<State, SubState2>,
+  selector3: Selector<State, SubState3>,
   projector: suspend (subState1: SubState1, subState2: SubState2, subState3: SubState3) -> Result
 ): Flow<Result> = selectInternal(
   selectors = arrayOf(
@@ -103,11 +149,25 @@ public fun <State, SubState1, SubState2, SubState3, Result> Flow<State>.select(
   }
 )
 
+/**
+ * Select four sub-states from the source [Flow] and combine them into a [Result].
+ *
+ * The [projector] will be invoked only when one of the sub-states is changed.
+ * The returned flow will emit the result of [projector],
+ * and all subsequent results repetitions of the same value are filtered out
+ * (same as [distinctUntilChanged]).
+ *
+ * @param selector1 The first selector to be used to select first sub-states.
+ * @param selector2 The second selector to be used to select second sub-states.
+ * @param selector3 The third selector to be used to select third sub-states.
+ * @param selector4 The fourth selector to be used to select fourth sub-states.
+ * @param projector The projector to be used to combine the sub-states into a result.
+ */
 public fun <State, SubState1, SubState2, SubState3, SubState4, Result> Flow<State>.select(
-  selector1: suspend (State) -> SubState1,
-  selector2: suspend (State) -> SubState2,
-  selector3: suspend (State) -> SubState3,
-  selector4: suspend (State) -> SubState4,
+  selector1: Selector<State, SubState1>,
+  selector2: Selector<State, SubState2>,
+  selector3: Selector<State, SubState3>,
+  selector4: Selector<State, SubState4>,
   projector: suspend (subState1: SubState1, subState2: SubState2, subState3: SubState3, subState4: SubState4) -> Result
 ): Flow<Result> = selectInternal(
   selectors = arrayOf(
@@ -126,13 +186,34 @@ public fun <State, SubState1, SubState2, SubState3, SubState4, Result> Flow<Stat
   }
 )
 
+/**
+ * Select five sub-states from the source [Flow] and combine them into a [Result].
+ *
+ * The [projector] will be invoked only when one of the sub-states is changed.
+ * The returned flow will emit the result of [projector],
+ * and all subsequent results repetitions of the same value are filtered out
+ * (same as [distinctUntilChanged]).
+ *
+ * @param selector1 The first selector to be used to select first sub-states.
+ * @param selector2 The second selector to be used to select second sub-states.
+ * @param selector3 The third selector to be used to select third sub-states.
+ * @param selector4 The fourth selector to be used to select fourth sub-states.
+ * @param selector5 The fifth selector to be used to select fifth sub-states.
+ * @param projector The projector to be used to combine the sub-states into a result.
+ */
 public fun <State, SubState1, SubState2, SubState3, SubState4, SubState5, Result> Flow<State>.select(
-  selector1: suspend (State) -> SubState1,
-  selector2: suspend (State) -> SubState2,
-  selector3: suspend (State) -> SubState3,
-  selector4: suspend (State) -> SubState4,
-  selector5: suspend (State) -> SubState5,
-  projector: suspend (subState1: SubState1, subState2: SubState2, subState3: SubState3, subState4: SubState4, subState5: SubState5) -> Result
+  selector1: Selector<State, SubState1>,
+  selector2: Selector<State, SubState2>,
+  selector3: Selector<State, SubState3>,
+  selector4: Selector<State, SubState4>,
+  selector5: Selector<State, SubState5>,
+  projector: suspend (
+    subState1: SubState1,
+    subState2: SubState2,
+    subState3: SubState3,
+    subState4: SubState4,
+    subState5: SubState5
+  ) -> Result
 ): Flow<Result> = selectInternal(
   selectors = arrayOf(
     selector1,
